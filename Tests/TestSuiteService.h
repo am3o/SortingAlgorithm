7c951386005e0775851014f51a74ae6d4e4ebc0d
Joshua Braun <joshuabraun@web.de>
2015-11-10 18:44:19 +0100

    Test-Daten werden über den TestSuiteService erstellt

---
+++ Tests/TestSuiteService.h
@@ -0,0 +1,58 @@
+//
+// Created by am3o on 10.11.15.
+//
+
+#ifndef SORTINGALGORITHM_TESTSUITESERVICE_H
+#define SORTINGALGORITHM_TESTSUITESERVICE_H
+
+
+#include <stdlib.h>
+#include <iterator>
+#include <algorithm>
+#include <array>
+
+class TestSuiteService {
+public:
+    TestSuiteService();
+    TestSuiteService(const TestSuiteService & orig);
+    virtual ~TestSuiteService();
+
+    template<typename T, size_t SIZE>
+    void fillArrayRandom(std::array<T, SIZE> *ptr);
+
+    template<typename T, size_t SIZE>
+    void fillArrayDesc(std::array<T, SIZE> *ptr);
+
+    template<typename T, size_t SIZE>
+    void fillArrayAsc(std::array<T, SIZE> *ptr);
+private:
+};
+
+TestSuiteService::TestSuiteService(){ }
+
+TestSuiteService::TestSuiteService(const TestSuiteService & orig) { }
+
+TestSuiteService::~TestSuiteService() { }
+
+template<typename T, size_t SIZE>
+void TestSuiteService::fillArrayAsc(std::array<T, SIZE> *ptr) {
+    for(size_t i = 0; i < SIZE; i++){
+        ptr->at(i) = (T) i;
+    }
+}
+
+template<typename T, size_t SIZE>
+void TestSuiteService::fillArrayDesc(std::array<T, SIZE> *ptr) {
+    for(size_t i = 0; i < SIZE; i++){
+        ptr->at(i) = SIZE - i;
+    }
+}
+
+template<typename T, size_t SIZE>
+void TestSuiteService::fillArrayRandom(std::array<T, SIZE> *ptr) {
+    for(size_t i = 0; i < SIZE; i++){
+        ptr->at(i)= (T) rand()%100;
+    }
+}
+
+#endif //SORTINGALGORITHM_TESTSUITESERVICE_H
