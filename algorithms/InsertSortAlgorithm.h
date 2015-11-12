08fdbd65c8d4f4936b697893c7e7c6420520d93f
Joshua Braun <joshuabraun@web.de>
2015-11-12 10:33:53 +0100

    Anpassung der Ordner-Namen

---
+++ algorithms/InsertSortAlgorithm.h
@@ -0,0 +1,57 @@
+//
+// Created by am3o on 10.11.15.
+//
+
+#ifndef SORTINGALGORITHM_INSERTSORTALGORITHM_H
+#define SORTINGALGORITHM_INSERTSORTALGORITHM_H
+
+
+#include "SortingAlgorithmBase.h"
+
+
+class InsertSortAlgorithm : SortingAlgorithmBase {
+public:
+    InsertSortAlgorithm();
+    InsertSortAlgorithm(const InsertSortAlgorithm& orig);
+    virtual ~InsertSortAlgorithm();
+
+    template <typename T, size_t SIZE>
+    void sort(std::array<T, SIZE>* ptr);
+
+    template <typename T, size_t SIZE>
+    void sortWithGuardian(std::array<T, SIZE>* ptr);
+};
+
+InsertSortAlgorithm::InsertSortAlgorithm(){
+
+}
+
+InsertSortAlgorithm::~InsertSortAlgorithm(){
+
+}
+
+template<typename T, size_t SIZE>
+void InsertSortAlgorithm::sort(std::array<T, SIZE>* ptr) {
+    for(size_t i = 1; i < SIZE; i++) {
+        T tempValue = ptr->at(i);
+        for(size_t j = i; j > 0 && tempValue < ptr->at(j-1); j--) {
+            ptr->at(j) = ptr->at(j-1);
+            ptr->at(j-1) = tempValue;
+        }
+    }
+}
+
+template<typename T, size_t SIZE>
+void InsertSortAlgorithm::sortWithGuardian(std::array<T, SIZE>* ptr) {
+    T* guard = this->findMinimumOptimized(ptr);
+    std::swap(ptr->at(0), *guard);
+    for(size_t i = 1; i < SIZE; i++) {
+        T tempValue = ptr->at(i);
+        for(size_t j = i; tempValue < ptr->at(j-1); j--) {
+            ptr->at(j) = ptr->at(j-1);
+            ptr->at(j-1) = tempValue;
+        }
+    }
+}
+
+#endif //SORTINGALGORITHM_INSERTSORTALGORITHM_H
---
+++ algorithms/SortingAlgorithmBase.h
@@ -0,0 +1,65 @@
+//
+// Created by am3o on 10.11.15.
+//
+
+#ifndef SORTINGALGORITHM_SORTINGALGORITHMBASE_H
+#define SORTINGALGORITHM_SORTINGALGORITHMBASE_H
+
+#include <stdlib.h>
+#include <iterator>
+#include <algorithm>
+#include <array>
+
+class SortingAlgorithmBase {
+public:
+    SortingAlgorithmBase();
+    virtual ~SortingAlgorithmBase();
+protected:
+    template<typename T, size_t SIZE>
+    T* findMinimumOptimized (std::array<T, SIZE> *ptr);
+
+    template<typename T, size_t SIZE>
+    T* findMinimumOptimized (std::array<T, SIZE> *ptr, size_t startIndex);
+
+    template<typename T, size_t SIZE>
+    T* findMinimumUnoptimized (std::array<T, SIZE> *ptr);
+
+    template<typename T, size_t SIZE>
+    T* findMinimumUnoptimized (std::array<T, SIZE> *ptr, size_t startIndex);
+};
+
+SortingAlgorithmBase::SortingAlgorithmBase() { }
+
+SortingAlgorithmBase::~SortingAlgorithmBase() { }
+
+template<typename T, size_t SIZE>
+T* SortingAlgorithmBase::findMinimumOptimized(std::array<T, SIZE>* ptr) {
+    return this->findMinimumOptimized(ptr, 0);
+}
+
+template<typename T, size_t SIZE>
+T* SortingAlgorithmBase::findMinimumOptimized(std::array<T, SIZE>* ptr, size_t startIndex) {
+    T* minValue = &ptr->at(startIndex);
+    for(size_t i = startIndex; i < SIZE; i++){
+        if(*minValue > ptr->at(i))
+            minValue = &ptr->at(i);
+    }
+    return minValue;
+}
+
+template<typename T, size_t SIZE>
+T* SortingAlgorithmBase::findMinimumUnoptimized(std::array<T, SIZE>* ptr) {
+    return this->findMinimumUnoptimized(ptr, 0);
+}
+
+template<typename T, size_t SIZE>
+T* SortingAlgorithmBase::findMinimumUnoptimized(std::array<T, SIZE>* ptr, size_t startIndex) {
+    T* minValue = &ptr->at(startIndex);
+    for(size_t i = SIZE-1; i >= startIndex; i--){
+        if(*minValue > ptr->at(i))
+            minValue = &ptr->at(i);
+    }
+    return minValue;
+}
+
+#endif //SORTINGALGORITHM_SORTINGALGORITHMBASE_H
