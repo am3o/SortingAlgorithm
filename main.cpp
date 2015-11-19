446ddd9f1637f69b2b856001fbe3c127fd636ef6
Joshua Braun <joshuabraun@web.de>
2015-11-19 16:49:14 +0100

    Console wird als Ausgabestream genutzt

--- main.cpp
+++ main.cpp
@@ -3,19 +3,32 @@
  */
 
 #include <iostream>
+#include <fstream>
 
 #include "benchmark/BenchmarkRunner.h"
 
 #define ELEMENTTYPE double
-#define SIZE 8000
+#define SIZE 4000
+
+using namespace std;
 
 int main() {
-    std::array<ELEMENTTYPE, SIZE>* i_array = new std::array<ELEMENTTYPE, SIZE>();
+    std::ostream* stream;
 
-    BenchmarkRunner runner;
-    runner.runSelectionSortAlgorithm(i_array);
-    runner.runInsertSortAlgorithm(i_array);
+    //ofstream recordFile;
+    //recordFile.open ("Record.csv");
 
+    stream = &std::cout;
+    *stream << "Algorithm;Elements;Random [s]; ASC [s]; DESC [s]" << endl;
+
+    BenchmarkRunner runner(stream);
+
+    std::array<ELEMENTTYPE, SIZE>* small_array = new std::array<ELEMENTTYPE, SIZE>();
+
+    runner.runSelectionSortAlgorithm(*small_array);
+    runner.runInsertSortAlgorithm(*small_array);
+
+    //recordFile.close();
 
     return 0;
 }
