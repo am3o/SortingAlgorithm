604a807a31a5c7473ba2ab02cb9d9b64a1320c94
Joshua Braun <joshuabraun@web.de>
2015-12-29 15:16:04 +0100

    Quicksort optimiert für worst-case

--- algorithm/QuickSortAlgorithm.h
+++ algorithm/QuickSortAlgorithm.h
@@ -11,55 +11,51 @@
 class QuicksortAlgorithm : public SortingAlgorithmBase {
 private:
     template <typename T, size_t SIZE>
-    int partition(std::array<T, SIZE>& elements, int lo, int hi);
+    void internalSort(std::array<T, SIZE>& elements, size_t lo, size_t hi);
 public:
     template <typename T, size_t SIZE>
-    void sort(std::array<T, SIZE>& elements, int lo, int hi);
+    void sort(std::array<T, SIZE>& elements, size_t lo, size_t hi);
 
     template <typename T, size_t SIZE>
-    void quicksort(std::array<T, SIZE>& elements, int lo, int hi);
+    void sort(std::array<T, SIZE>& elements);
 };
 
 template <typename T, size_t SIZE>
-int QuicksortAlgorithm::partition(std::array<T, SIZE> &elements, int lo, int hi) {
-    int i = lo, j = hi+1;
-    T v = elements[lo];
-    while(true){
-        while(elements[++i] < v) if(i == hi) break;
-        while(v < elements[--j]) if(j == lo) break;
+void QuicksortAlgorithm::internalSort(std::array<T, SIZE> &elements, size_t lo, size_t hi) {
+    size_t i = lo - 1, j = hi;
+    size_t p = lo, q = hi-1;
+
+    std::swap(elements[(lo + hi) / 2], elements[hi]);
+    T pivot = elements[hi];
+
+    if(hi <= lo) return;
+    while(true) {
+        while(elements[++i] < pivot);
+        while(pivot < elements[--j]) { if(j==lo) break; }
         if(i >= j) break;
         std::swap(elements[i], elements[j]);
+
+        if(elements[i] == pivot) { std::swap(elements[p], elements[i]); p++;}
+        if(pivot == elements[j]) { std::swap(elements[j], elements[q]); q--;}
     }
-    std::swap(elements[lo], elements[j]);
-    return j;
+
+    std::swap(elements[i], elements[hi]);
+    i == 0 ? j = 0 : j = i-1;
+    i=i+1;
+
+    size_t k = lo;
+    while(k < p) { std::swap(elements[k], elements[j]); k++; if(j>0) j--; }
+    k = hi-1;
+    while(k > q){ std::swap(elements[i], elements[k]); i++; if(k>0) k--; }
+
+    internalSort(elements, lo, j);
+    internalSort(elements, i, hi);
 }
 
 template <typename T, size_t SIZE>
-void QuicksortAlgorithm::sort(std::array<T, SIZE> &elements, int lo, int hi) {
-    if(hi <= lo) return;
-    int j = partition(elements, lo, hi);
-    this->sort(elements, lo, j-1);
-    this->sort(elements, j+1, hi);
+void QuicksortAlgorithm::sort(std::array<T, SIZE> &elements) {
+    return this->internalSort(elements, 0, SIZE-1);
 }
 
-template <typename T, size_t SIZE>
-void QuicksortAlgorithm::quicksort(std::array<T, SIZE> &elements, int l, int r) {
-    int i = l-1, j = r, p = l-1, q = r; T v = elements[r];
-    if (r <= l) return;
-    for (;;)
-    {
-        while (elements[++i] < v) ;
-        while (v < elements[--j]) if (j == l) break;
-        if (i >= j) break;
-        std::swap(elements[i], elements[j]);
-        if (elements[i] == v) { p++; std::swap(elements[p], elements[i]); }
-        if (v == elements[j]) { q--; std::swap(elements[j], elements[q]); }
-    }
-    std::swap(elements[i], elements[r]); j = i-1; i = i+1;
-    for (int k = l; k < p; k++, j--) std::swap(elements[k], elements[j]);
-    for (int k = r-1; k > q; k--, i++) std::swap(elements[i], elements[k]);
-    quicksort(elements, l, j);
-    quicksort(elements, i, r);
-}
 
 #endif //SORTINGALGORITHM_QUICKSORTALGORITHM_H
