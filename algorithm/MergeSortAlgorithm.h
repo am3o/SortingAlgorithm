871c50247f3b2335b805bb85ca5728a729eec44b
Joshua Braun <joshuabraun@web.de>
2015-12-29 15:06:44 +0100

    natural Mergesort implementiert [not stabil]

--- algorithm/MergeSortAlgorithm.h
+++ algorithm/MergeSortAlgorithm.h
@@ -11,7 +11,10 @@
 class MergeSortAlgorithm : public SortingAlgorithmBase {
 private:
     template <typename T, size_t SIZE>
-    void merge(std::array<T, SIZE> &a, std::array<T, SIZE> &aux,  int lo, int hi, bool asc);
+    void merge(std::array<T, SIZE> &a, std::array<T, SIZE> &aux,  size_t lo, size_t hi, bool asc);
+
+    template <typename T, size_t SIZE>
+    void merge(std::array<T, SIZE> &a, std::array<T, SIZE> &aux,  size_t lo, size_t hi);
 
     template <typename  T, size_t SIZE>
     bool mergeruns(std::array<T, SIZE>& elements, std::array<T, SIZE>& aux);
@@ -25,26 +28,11 @@ public:
 };
 
 template <typename T, size_t SIZE>
-void MergeSortAlgorithm::merge(std::array<T, SIZE> &a, std::array<T, SIZE> &aux,  int lo, int hi, bool asc) {
-    int i = lo, mid = lo + (hi - lo)/2, j = mid + 1;
-
-    for(int k = lo ;k <= hi; k++ )
-        aux[k] = a[k];
-
-    for(int k = lo; k <= hi; k++){
-        if      (i > mid)   a[k] = aux[j++];
-        else if (j > hi)    a[k] = aux[i++];
-        else if (aux[j] < aux[i]) a[k] = aux[j++];
-        else a[k] = aux[i++];
-    }
-}
-
-template <typename T, size_t SIZE>
 void MergeSortAlgorithm::sortBottomUp(std::array<T, SIZE> &elements) {
     std::array<T, SIZE>* aux = new std::array<T, SIZE>();
     for(long unsigned sz = 1; sz < SIZE ; sz = sz + sz){
         for(long unsigned lo = 0; lo < SIZE - sz; lo += sz + sz){
-            merge(elements, *aux, lo, std::min(lo+sz+sz-1, SIZE-1), true);
+            this->merge(elements, *aux, lo, std::min(lo+sz+sz-1, SIZE-1));
         }
     }
     delete aux;
@@ -53,21 +41,52 @@ void MergeSortAlgorithm::sortBottomUp(std::array<T, SIZE> &elements) {
 template  <typename T, size_t SIZE>
 void MergeSortAlgorithm::sortNatural(std::array<T, SIZE> &elements) {
     std::array<T, SIZE>* aux = new std::array<T, SIZE>();
-    while(!mergeruns(elements, *aux) && !mergeruns(*aux, elements));
+    while(!mergeruns(elements, *aux) & !mergeruns(*aux, elements));
 }
 
 template <typename T, size_t SIZE>
 bool MergeSortAlgorithm::mergeruns(std::array<T, SIZE> &elements, std::array<T, SIZE>& aux) {
-    size_t k = 0;
-    T value;
+    size_t i=0, k=0, x;
     bool asc = true;
-    for(size_t i = 0; i < SIZE; k = i){
-        do value = elements[i++]; while (i < SIZE && value <= elements[i]);
-        while(i < SIZE && value >= elements[i]) value = elements[i++];
-        merge(elements, aux, k, std::min(i-1, SIZE-1), asc);
-        asc =! asc;
+
+    while (i<SIZE) {
+        k=i;
+        do x=elements[i++]; while (i<SIZE && x<=elements[i]);
+        while (i<SIZE && x>=elements[i]) x=elements[i++];
+        merge (elements, aux, k, i-1, asc);
+        asc=!asc;
     }
-    return k == 0;
+    return k==0;
+}
+
+template <typename T, size_t SIZE>
+void MergeSortAlgorithm::merge(std::array<T, SIZE> &a, std::array<T, SIZE> &aux,  size_t lo, size_t hi, bool asc) {
+    int k=asc? lo: hi;
+    int c=asc? 1: -1;
+    int i=lo, j=hi;
+
+    while (i<=j) {
+        if (a[i]<=a[j])
+            aux[k]=a[i++];
+        else
+            aux[k]=a[j--];
+        k+=c;
+    }
+}
+
+template <typename T, size_t SIZE>
+void MergeSortAlgorithm::merge(std::array<T, SIZE> &a, std::array<T, SIZE> &aux,  size_t lo, size_t hi) {
+    size_t i = lo, mid = lo + (hi - lo)/2, j = mid + 1;
+
+    for(size_t k = lo ;k <= hi; k++ )
+        aux[k] = a[k];
+
+    for(size_t k = lo; k <= hi; k++){
+        if      (i > mid)   a[k] = aux[j++];
+        else if (j > hi)    a[k] = aux[i++];
+        else if (aux[j] < aux[i]) a[k] = aux[j++];
+        else a[k] = aux[i++];
+    }
 }
 
 
